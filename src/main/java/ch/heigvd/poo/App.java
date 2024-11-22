0406bc8ad73e1c2f46f91ff8305d251bf67131b7
Jonatan Perret <perretjonatan1@gmail.com>
2024-11-22 10:44:11 +0100

    converted to maven project

---
+++ src/main/java/ch/heigvd/poo/App.java
@@ -0,0 +1,15 @@
+package ch.heigvd.poo;
+
+import ch.heigvd.poo.calculator.JCalculator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        new JCalculator();
+    }
+}
---
+++ src/main/java/ch/heigvd/poo/calculator/Operator.java
@@ -0,0 +1,6 @@
+package ch.heigvd.poo.calculator;
+
+abstract class Operator
+{
+  abstract void execute();
+}
